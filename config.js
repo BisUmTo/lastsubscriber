c74d292bca5ed51c56a7d7d96a93f75b479d4cd5
BisUmTo <bisumto@gmail.com>
2025-08-11 19:23:06 +0000

    Deploying to gh-pages from @ BisUmTo/lastsubscriber@2f8c36d5f902feb3b97932deee9f354e2e9d49b8 🚀

---
+++ config.js
@@ -0,0 +1,3 @@
+const CLIENT_ID = '606667923653-7n7u6qon2udh595tb2m9f1el6iv9g18c.apps.googleusercontent.com';
+const REDIRECT_URI = 'https:// BisUmTo.github.io/lastsubscriber/auth.html';
+const SCOPE = 'https://www.googleapis.com/auth/youtube.readonly';
